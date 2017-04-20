ff4729dae7b02a90dfa95e835d35cb55c33d9e04
John Spicer <jspicer@stevens.edu>
2017-04-20 19:18:24 -0400

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const homeRoute = require("./home");
 const accountRoute = require("./account");
 const categoryRoute = require("./categories");
+const experienceRoute = require("./experience");
 
 const constructorMethod = (app) => {
     app.use("/", homeRoute);
     app.use("/account", accountRoute);
     app.use("/categories", categoryRoute);
+    app.use("/experience", experienceRoute);
     app.use("*", (req, res) => {
         res.status(404).json({
             error: "Not found"
